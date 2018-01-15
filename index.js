ec0ce45c8326bee42f793153c13adbace420159e
Dipti Nayan <diptinayan21@gmail.com>
2018-01-15 15:19:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-function johnLennonFacts() {
-  const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-while (true) {
-  return johnLennonFacts(facts + "!!!") 
+function theBeatlesPlay(musicians,instruments){
+  var i=0;
+  var j=0;
+  var array=[];
+  for (i=0;i<musicians.length;i++){
+    var person=musicians[i];
+    var instrumentEnd=instruments[i];
+    var complete= person + " plays " + instrumentEnd;
+    array.push(complete);
+  }
+  console.log("firstArray",array,complete)
+  return array
 
 }
+theBeatlesPlay(musicians,instruments)
 
+
+function johnLennonFacts(facts){
+  var f=0;
+  var finalFacts=[]
+  while (f<facts.length){
+    var excitingFacts=facts[f]+"!!!";
+    finalFacts.push(excitingFacts)
+    f++
   }
+  console.log("While !!!",finalFacts)
+
+  return finalFacts
+}
+johnLennonFacts(facts)
+
+
+function iLoveTheBeatles(number){
+  var array=[];
+  do{
+    array.push("I love the Beatles!");
+    number++;
+  }
+  while (number<15);
+  // console.log("Final: ",array);
+  return array;
+}
+iLoveTheBeatles(number)
